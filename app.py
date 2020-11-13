f7e6afdd9c84b04fc5d11e1355de9bc77468ddbc
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2020-11-13 19:29:20 +0545

    added endopoints to list image names and images

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, render_template
+from flask.helpers import send_from_directory
 
 app = Flask(__name__)
 
@@ -10,3 +11,12 @@ import os
 def home():
   folders = os.listdir(f'{os.getcwd()}{os.sep}thumbnails')
   return render_template('home.html', folders = folders)
+
+@app.route('/<path:folder>')
+def folder(folder):
+  files = os.listdir(f'{os.getcwd()}{os.sep}thumbnails{os.sep}{folder}')
+  return render_template('images.html', folder = folder, files = files)
+
+@app.route('/thumbnails/<path:path>')
+def thumbnails(path):
+  return send_from_directory('thumbnails', path)
