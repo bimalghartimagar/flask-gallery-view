d01c2939ab2a0e6f2d7c71a03519e9c6e5630008
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2020-11-17 20:35:48 +0545

    added ip logging and favicon resolver

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import os
-
-from flask import Flask, render_template
+from datetime import datetime
+from flask import Flask, render_template, request
 from flask.helpers import send_from_directory
 
 app = Flask(__name__)
 
-import os
+f = open("ip_log.txt", "a")
 
 @app.route('/')
 def home():
+  f.write(f"HOME: {request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)} \t {str(datetime.now())} \n")
   folders = os.listdir(f'{os.getcwd()}{os.sep}thumbnails')
   return render_template('home.html', folders = folders)
 
 @app.route('/<path:folder>')
 def folder(folder):
+  f.write(f"PATH: {folder} \t {request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)} \t {str(datetime.now())} \n")
+  if 'favicon' in str(folder):
+    return ('', 204)
   files = os.listdir(f'{os.getcwd()}{os.sep}thumbnails{os.sep}{folder}')
   return render_template('images.html', folder = folder, files = files)
 
@@ -23,5 +27,6 @@ def thumbnails(path):
 
 @app.route('/download/<path:path>')
 def download(path):
+  f.write(f"DOWNLOAD: {path} \t {request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)} \t {str(datetime.now())} \n")
   gallery_path = ""
   return send_from_directory(gallery_path, path)
